ebab463e70c973fa0086e1ec47bccfebcb1203f8
manishaSoni8 <manishasoni@startbitsolutions.com>
2025-04-15 19:11:31 +0530

    test

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const fs= require('fs');
-const https=require('https');
-
+const fs = require('fs');
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,34 +10,39 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
-const helmet =require('helmet');
-const morgan =require('morgan');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
+const compression = require('compression');
+ 
 const errorController = require('./controllers/error');
 const shopController = require('./controllers/shop');
 const isAuth = require('./middleware/is-auth');
 const User = require('./models/user');
-
-const MONGODB_URI =
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@nodejs.2g1go.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+ 
 const app = express();
+ 
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@nodejs.2g1go.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
+ 
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+ 
+store.on('error', function (error) {
+  console.error('Session Store Error:', error);
+});
+ 
 const csrfProtection = csrf();
-const privateKey = fs.readFileSync('server.key');
-const certificate=fs.readFileSync('server.cert');   
-
+ 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'images');
+  destination: function (req, file, cb) {
+    cb(null, 'images');
   },
-  filename: function(req, file, cb) {
-      cb(null, uuidv4())
+  filename: function (req, file, cb) {
+    cb(null, uuidv4());
   }
 });
-
+ 
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/png' ||
@@ -51,27 +54,26 @@ const fileFilter = (req, file, cb) => {
     cb(null, false);
   }
 };
-
+ 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
+ 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
-const compression = require('compression');
-
-const accessLogStream= fs.createWriteStream(path.join(__dirname, 'access.log'),{flags: 'a'});
-
+ 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+ 
 app.use(helmet());
 app.use(compression());
-app.use(morgan('combined',{stream:accessLogStream}));
-
+app.use(morgan('combined', { stream: accessLogStream }));
+ 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  multer({ storage: storage, fileFilter: fileFilter }).single('image')
-);
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
+ 
 app.use(
   session({
     secret: 'my secret',
@@ -80,16 +82,15 @@ app.use(
     store: store
   })
 );
-
+ 
 app.use(flash());
-
+ 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   next();
 });
-
+ 
 app.use((req, res, next) => {
-  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
@@ -105,39 +106,44 @@ app.use((req, res, next) => {
       next(new Error(err));
     });
 });
-
+ 
 app.post('/create-order', isAuth, shopController.postOrder);
-
+ 
 app.use(csrfProtection);
+ 
 app.use((req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 });
-
+ 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
+ 
 app.get('/500', errorController.get500);
-
 app.use(errorController.get404);
-
-// app.use((error, req, res, next) => {
-//   // res.status(error.httpStatusCode).render(...);
-//   // res.redirect('/500');
-//   res.status(500).render('500', {
-//     pageTitle: 'Error!',
-//     path: '/500',
-//     isAuthenticated: req.session.isLoggedIn
-//   });
-// });
-
+ 
+// Global error handler
+app.use((error, req, res, next) => {
+  console.error('Unhandled Error:', error);
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session ? req.session.isLoggedIn : false
+  });
+});
+ 
+// Connect to DB and start server
 mongoose
-  .connect(MONGODB_URI)
-  .then(result => {
-    app.listen(process.env.PORT || 3000);
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true // Important for MongoDB Atlas on Render
+  })
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   })
   .catch(err => {
-    console.log(err);
+    console.error('DB connection failed:', err);
   });
-
