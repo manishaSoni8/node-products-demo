32d678334ee2633f91306479c3abbb5e9f286ebb
manishaSoni8 <manishasoni@startbitsolutions.com>
2025-04-16 09:47:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs');
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,36 +12,29 @@ const flash = require('connect-flash');
 const multer = require('multer');
 
 
- 
 const errorController = require('./controllers/error');
 const shopController = require('./controllers/shop');
 const isAuth = require('./middleware/is-auth');
 const User = require('./models/user');
- 
+
+const MONGODB_URI =
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@nodejs.2g1go.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 const app = express();
- 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@nodejs.2g1go.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
- 
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
- 
-store.on('error', function (error) {
-  console.error('Session Store Error:', error);
-});
- 
 const csrfProtection = csrf();
- 
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'images');
+  destination: function(req, file, cb) {
+      cb(null, 'images');
   },
-  filename: function (req, file, cb) {
-    cb(null, uuidv4());
+  filename: function(req, file, cb) {
+      cb(null, uuidv4())
   }
 });
- 
+
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/png' ||
@@ -53,24 +46,21 @@ const fileFilter = (req, file, cb) => {
     cb(null, false);
   }
 };
- 
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
- 
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
- 
 
- 
 
- 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
- 
+app.use(
+  multer({ storage: storage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
- 
 app.use(
   session({
     secret: 'my secret',
@@ -79,15 +69,16 @@ app.use(
     store: store
   })
 );
- 
+
 app.use(flash());
- 
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   next();
 });
- 
+
 app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
@@ -103,34 +94,33 @@ app.use((req, res, next) => {
       next(new Error(err));
     });
 });
- 
+
 app.post('/create-order', isAuth, shopController.postOrder);
- 
+
 app.use(csrfProtection);
- 
 app.use((req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 });
- 
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
- 
+
 app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
- 
-// Global error handler
-app.use((error, req, res, next) => {
-  console.error('Unhandled Error:', error);
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session ? req.session.isLoggedIn : false
-  });
-});
- 
-// Connect to DB and start server
+
+// app.use((error, req, res, next) => {
+//   // res.status(error.httpStatusCode).render(...);
+//   // res.redirect('/500');
+//   res.status(500).render('500', {
+//     pageTitle: 'Error!',
+//     path: '/500',
+//     isAuthenticated: req.session.isLoggedIn
+//   });
+// });
+
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
@@ -139,3 +129,4 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
